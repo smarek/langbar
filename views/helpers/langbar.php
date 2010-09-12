0643304140dc9b1158d07d979e11b55b1b9805dc
Your Name <marekk.sebera@gmail.com>
2010-09-12 12:59:39 +0200

    Type&Slug repair

--- views/helpers/langbar.php
+++ views/helpers/langbar.php
@@ -10,11 +10,11 @@ class LangbarHelper extends AppHelper {
     function getLangs() {
 	if(($langs = Cache::read('langbar.langs')) === false){
         $this->Language = & ClassRegistry::init('Language');
-        $slug = isset($this->params["named"]["slug"]) ? $this->params["named"]["slug"] : "";
-        $type = isset($this->params["named"]["type"]) ? $this->params["named"]["type"] : "";
 	$langs = $this->Language->find('all');
 	Cache::write('langbar.langs', $langs);
 	}
+        $slug = isset($this->params["named"]["slug"]) ? $this->params["named"]["slug"] : "";
+        $type = isset($this->params["named"]["type"]) ? $this->params["named"]["type"] : "";
         return array($langs, $slug, $type);
     }
 
