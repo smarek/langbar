b8b7e1812af8c10a375e99ad0b0042bdeac8f5dc
Your Name <marekk.sebera@gmail.com>
2010-09-12 12:55:49 +0200

    Caching for speed

--- views/helpers/langbar.php
+++ views/helpers/langbar.php
@@ -8,10 +8,14 @@ class LangbarHelper extends AppHelper {
     );
 
     function getLangs() {
+	if(($langs = Cache::read('langbar.langs')) === false){
         $this->Language = & ClassRegistry::init('Language');
         $slug = isset($this->params["named"]["slug"]) ? $this->params["named"]["slug"] : "";
         $type = isset($this->params["named"]["type"]) ? $this->params["named"]["type"] : "";
-        return array($this->Language->find('all'), $slug, $type);
+	$langs = $this->Language->find('all');
+	Cache::write('langbar.langs', $langs);
+	}
+        return array($langs, $slug, $type);
     }
 
 }
